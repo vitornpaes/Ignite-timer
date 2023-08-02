e86b73cf41be58a9668e7a0796c2fb5c9d0ff8be
VitorPaes <vitornoepaes@hotmail.com>
2023-08-02 10:27:18 -0300

    parte 2 completed

--- src/global.ts
+++ src/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyled = createGlobalStyle`
   body{
     background: ${(props) => props.theme["gray-900"]};
     color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input-security, textarea, button {
@@ -22,4 +23,4 @@ export const GlobalStyled = createGlobalStyle`
     font-weight: 400;
     font-size: 1rem;
   }
-`;  
+`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,64 @@
-export function Home(){
-  return (
-    <div>
-      <form action="">
-        <label htmlFor="">Vou trabalhar em</label>
-        <input id="task" />
-        <label htmlFor="">durante</label>
-        <input type="number" id="minutesAmount" />
-        <span>minutos.</span>
-      </form>
-    </div>
-  )
+import { Play } from "phosphor-react";
+import {
+  CountdownStyled,
+  FormStyled,
+  HomeStyled,
+  Separator,
+  MinutesAmountInput,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+import { useState } from "react";
 
+export function Home() {
+  const [task, setTask] = useState("");
+
+  function handleTaskChange(event){
+    setTask(event.target.value);
+  }
+
+  return (
+    <HomeStyled>
+      <form action="">
+        <FormStyled>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            value={task}
+            onChange={handleTaskChange}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label htmlFor="">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+          <span>minutos.</span>
+        </FormStyled>
+        <CountdownStyled>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownStyled>
+        <StartCountdownButton disabled={!task.trim()} type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeStyled>
+  );
 }
